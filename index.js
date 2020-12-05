a0e3993f22a2cb687e1e9185d879ed9010f1e772
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-05 17:55:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,10 @@ fetch("http://localhost:3000/dogs", {
     dogBreed: "Poodle"
   })
 });
+fetch("http://localhost:3000/dogs", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+  });
