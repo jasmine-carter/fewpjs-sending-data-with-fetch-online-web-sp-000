5b0db86d3da0b9b9b966fcd981053754a2c95675
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-05 18:16:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-function submitData( name, email ) {
-  return fetch( 'http://localhost:3000/users', {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-      body: JSON.stringify( {
-        name,
-        email
-      } )
-    } )
-    .then( function ( response ) {
-      return response.json()
-    } )
-    .then( function ( object ) {
-      document.body.innerHTML = object[ "id" ]
-    } )
-    .catch( function ( error ) {
-      document.body.innerHTML = error.message
-    } )
-}
+function submitData(name, email) {
+  fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name,
+      email
+    })
+  })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      document.body.innerHTML = object["id"];
+    })
+    .catch(function(error) {
+      alert("Bad things! Ragnarők!");
+      document.body.textContent = error.message;
+    })
+  }
