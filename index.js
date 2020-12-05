7cbbf64dd7b2a7ac6525dc4480f916ca00051240
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-05 17:51:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Add your code here
+fetch("http://localhost:3000/dogs", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify({
+    dogName: "Byron",
+    dogBreed: "Poodle"
+  })
+});
