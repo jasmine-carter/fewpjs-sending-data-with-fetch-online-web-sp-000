6af05d30e2fb9d2ebdbc9b0ac5f978b8090e672f
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-05 18:22:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ function submitData(name, email) {
   })
     .then(function(response) {
       return response.json()
-    })
+    } )
     .then(function(object) {
       document.body.innerHTML = object["id"]
-    })
+    } )
     .catch(function(error) {
       document.body.innerHTML = error.message
     })
